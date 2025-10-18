872629419f7635b995f1aa7161a3d0ddd4231dcd
root <root@DESKTOP-KER2K7P.localdomain>
2025-10-18 01:18:24 +0100

    Added Upcoming movies page.

---
+++ movies/src/components/cardIcons/playlistAdd.jsx
@@ -0,0 +1,2 @@
+// Placeholder removed for Exercise 1-only commit. File kept intentionally blank.
+export default function PlaylistAddIcon() { return null; }
---
+++ movies/src/pages/upcomingMoviesPage.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getUpcomingMovies } from "../api/tmdb-api";
+import PageTemplate from "../components/templateMovieListPage";
+import AddToFavoritesIcon from "../components/cardIcons/addToFavorites";
+import Spinner from "../components/spinner";
+
+const UpcomingMoviesPage = () => {
+  const { data, error, isPending, isError } = useQuery({
+    queryKey: ["upcoming"],
+    queryFn: getUpcomingMovies,
+  });
+
+  if (isPending) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+
+  const movies = data.results;
+
+  return (
+    <PageTemplate
+      title="Upcoming Movies"
+      movies={movies}
+      action={(movie) => <AddToFavoritesIcon movie={movie} />}
+    />
+  );
+};
+
+export default UpcomingMoviesPage;
